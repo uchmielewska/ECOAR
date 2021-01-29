ef626b1f5ae7c645c6fd45826a3f2cbec7bf226d
Urszula Chmielewska <62251549+uchmielewska@users.noreply.github.com>
2021-01-30 00:44:28 +0100

    Update main.c

--- x86-32/main.c
+++ x86-32/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-extern void reduce_contrast(unsigned char *inputFileArray, int rfactor);
+extern void reduce_contrast(unsigned char *inputFileArray, unsigned int rfactor);
 
 int main(int argc, char *argv[])
 {
@@ -15,34 +15,38 @@ int main(int argc, char *argv[])
     if (argc <= 2)
     {
         printf("Too few arguments!\n");
+        return 0;
+    }
+
+    rfactor = atoi(argv[2]);
+    if (rfactor < 1 || rfactor > 127)
+    {
+        printf("Wrong rfactor\n");
+        return 0;
+    }
+
+    inputFile = fopen(argv[1], "rb");
+    if (inputFile == NULL)
+    {
+        printf("Cannot open the file %s \n", argv[1]);
     }
     else
     {
-        inputFile = fopen(argv[1], "rb");
-        if (inputFile == NULL)
-        {
-            printf("Cannot open the file %s \n", argv[1]);
-        }
-        else
-        {
-            outputFile = fopen("out.bmp", "wb");
-            fseek(inputFile, 0, SEEK_END);
-            fileSize = ftell(inputFile);
-            fseek(inputFile, 0, SEEK_SET);
+        outputFile = fopen("out.bmp", "wb");
+        fseek(inputFile, 0, SEEK_END);
+        fileSize = ftell(inputFile);
+        fseek(inputFile, 0, SEEK_SET);
 
-            inputFileArray = malloc(fileSize);
-            fread(inputFileArray, 1, fileSize, inputFile);
+        inputFileArray = malloc(fileSize);
+        fread(inputFileArray, 1, fileSize, inputFile);
 
-            rfactor = atoi(argv[2]);
+        reduce_contrast(inputFileArray, rfactor);
 
-            reduce_contrast(inputFileArray, rfactor);
-
-            fwrite(inputFileArray, 1, fileSize, outputFile);
-            fclose(outputFile);
-            fclose(inputFile);
-        }
+        fwrite(inputFileArray, 1, fileSize, outputFile);
+        fclose(outputFile);
+        fclose(inputFile);
     }
-
     return 0;
 }
+
 
