e3c2d52adb71d3266df0a0abd519600138c83dd5
Urszula Chmielewska <62251549+uchmielewska@users.noreply.github.com>
2021-02-01 21:06:08 +0100

    Update reduce_contrast.asm

--- x86-32/reduce_contrast.asm
+++ x86-32/reduce_contrast.asm
@@ -26,20 +26,17 @@ _reduce_contrast:
         mov     ch, [ebp+12]                    ;store rfactor in ch
 
         ;normWidth calculate
-        mov     eax, [edi+18]                   ;load width to eax
-        mov     esi, 0x3                        ;load 3 to edx
-        mul     esi                             ;width*3 (result is in eax)
-        mov     esi, eax                        ;store normWidth in esi
+        mov     ebx, [edi+18]                   ;load width to eax
+        lea     ebx, [ebx + ebx*2]              ;width*3 and store normWidth in esi
 
         ;padding calculate
-        and     eax, 0x3                        ;in eax there was already normWidth
-        mov     edx, 0x4                                  
-        sub     edx, eax
-        mov     cl, dl                          ;store padding in cl
+        and     bl, 3                           ;in eax there was already normWidth
+        mov     cl, 4                                  
+        sub     cl, bl                          ;store padding in cl
 
         ;set counters
         mov     edx, [edi+22]                   ;set rowCounter (edx) to height
-        mov     ebx, 0                          ;set columnCounter (ebx) to 0
+        xor     esi, esi                        ;set columnCounter (ebx) to 0
 
         ;move image pointer to the beginning of the bitmap (by the offset size)
         mov     eax, [edi+10]                   ;load offset to eax
@@ -47,22 +44,22 @@ _reduce_contrast:
 
 algorithm:
         mov     al, [edi]                       ;load current pixel to al
-        add     eax, -0x80                      ;pixel -= 128
+        add     eax, -128                       ;pixel -= 128
         imul    ch                              ;pixel *= rfactor
-        sar     eax, 0x7                        ;pixel /= 128
-        add     eax, 0x80                       ;pixel += 128
+        sar     eax, 7                          ;pixel /= 128
+        add     eax, 128                        ;pixel += 128
         mov     byte[edi], al                   ;update pixel in edi
         inc     edi                             ;go to the next pixel
 
-        inc     ebx                             ;columnCounter++
-        cmp     esi, ebx                        ;compare normWidth with columnCounter
+        inc     esi                             ;columnCounter++
+        cmp     ebx, esi                        ;compare normWidth with columnCounter
         jg      algorithm                       ;if (normWidth > columnCounter) continue with algorithm
 
 padding:
         dec     edx                             ;rowCounter--
         mov     al, [edi]                       ;load current pixel to al
         add     al, cl                          ;move image pointer by the number of padding bytes
-        mov     ebx, 0                          ;set columnCounter as 0
+        xor     esi, esi                        ;set columnCounter as 0
         cmp     edx, 0                          ;compare rowCounter with 0
         jg      algorithm                       ;if (rowCounter > 0) go to algorithm
 
